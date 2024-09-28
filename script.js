30edefbadd808517de1c86ba787d3674854358be
Abhijith Gaganan <theab10hd@gmail.com>
2024-09-28 10:14:09 +0530

    Logic updated

--- script.js
+++ script.js
@@ -1,62 +1,61 @@
 function register() {
-  if (username.value == "" || fname.value == "" || password.value == "") {
-    errorText.innerHTML = "Please fill the Form!";
-  } else {
-    if (confirmPassword.value != password.value) {
-      errorText.innerHTML = "Password Not Match!";
+  if (
+    username.value != "" &&
+    fname.value != "" &&
+    password.value != "" &&
+    confirmPassword.vlalue != ""
+  ) {
+    if (password.value != confirmPassword.value) {
+      errorText.innerHTML = `Password Dont Match!`;
     } else {
-      newUser = {
-        id: username.value,
-        name: fname.value,
-        email: email.value,
-        password: password.value,
-        balance: 0,
-      };
-
       if (localStorage.getItem(username.value)) {
-        alert("Username Exits");
+        errorText.innerHTML = `User Exits!`;
       } else {
-        localStorage.setItem(1, JSON.stringify(newUser));
+        user = {
+          id: username.value,
+          name: fname.value,
+          password: password.value,
+          balance: 0,
+        };
+
+        localStorage.setItem(username.value, JSON.stringify(user));
         alert("Account Created!");
-        window.location = "./dashboard.html";
+        window.location = "./login.html";
       }
     }
+  } else {
+    errorText.innerHTML = `Invalid credentials!`;
+  }
+}
+
+function login() {
+  if (username.value != "" && password.value != "") {
+    user = JSON.parse(localStorage.getItem(username.value));
+    if (user) {
+      if (user.password != password.value) {
+        errorText.innerHTML = `Password Invalid!`;
+      } else {
+        logginedUser = JSON.stringify(user);
+        localStorage.setItem("logginedUser", logginedUser);
+        alert("Login Success!");
+        window.location = "./dashboard.html";
+      }
+    } else {
+      errorText.innerHTML = `No User Found!`;
+    }
+  } else {
+    errorText.innerHTML = `Invalid credentials!`;
   }
 }
 
 function logout() {
-  localStorage.clear();
-  activeUser = "";
+  localStorage.removeItem("logginedUser");
+  alert("Logout Success!");
   window.location = "./index.html";
 }
 
-function login() {
-  if (username.value == "" || password.value == "") {
-    errorText.innerHTML = "Please fill the Form!";
-  } else {
-    textData = "";
-
-    if (!localStorage.getItem(username.value)) {
-      textData = "Username Didnt Exits!";
-    } else {
-      account = JSON.parse(localStorage.getItem(username.value));
-      console.log(account.password, password.value);
-
-      if (account.password != password.value) {
-        console.log(account);
-        textData = "Incorrect Password!";
-      } else {
-        alert("Login Success");
-        window.location = "./dashboard.html";
-      }
-    }
-
-    errorText.innerHTML = textData;
-  }
-}
-
 function deposit() {
-  user = JSON.parse(localStorage.getItem(1));
+  user = JSON.parse(localStorage.getItem("logginedUser"));
   if (dPassword.value == user.password) {
     if (dAmmount.value == "") {
       dErrorText.innerHTML = `Amount Incorrect!`;
@@ -64,7 +63,9 @@ function deposit() {
       alert("Amount Added!");
       user.balance += parseInt(dAmmount.value);
       amountBalance.innerHTML = `${user.balance}`;
-      localStorage.setItem(1, JSON.stringify(user));
+      localStorage.setItem("logginedUser", JSON.stringify(user));
+      updateUser();
+      dForm.reset();
     }
   } else {
     dErrorText.innerHTML = `Password Incorrect!`;
@@ -77,7 +78,7 @@ function deposit() {
 
 function withdraw() {
   amount = parseInt(wAmmount.value);
-  user = JSON.parse(localStorage.getItem(1));
+  user = JSON.parse(localStorage.getItem("logginedUser"));
   if (wAmmount.value == "") {
     wErrorText.innerHTML = `Amount Incorrect!`;
   } else if (amount > user.balance) {
@@ -87,7 +88,9 @@ function withdraw() {
       alert("Amount Withdrawn!");
       user.balance -= amount;
       amountBalance.innerHTML = `${user.balance}`;
-      localStorage.setItem(1, JSON.stringify(user));
+      localStorage.setItem("logginedUser", JSON.stringify(user));
+      updateUser();
+      wForm.reset();
     } else {
       wErrorText.innerHTML = `Password Incorrect!`;
     }
@@ -96,4 +99,9 @@ function withdraw() {
   setTimeout(() => {
     wErrorText.innerHTML = "";
   }, 3000);
+}
+
+function updateUser() {
+  logginedUser = JSON.parse(localStorage.getItem("logginedUser"));
+  localStorage.setItem(logginedUser.id, JSON.stringify(logginedUser));
 }
